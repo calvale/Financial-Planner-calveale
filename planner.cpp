2d1eca858a5192d69498a0d6c1a11bd53d33fc4c
calvale <calveale@oregonstate.edu>
2026-05-10 04:00:10 -0700

    Initial commit

---
+++ planner.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// Don't forget that your code needs to be broken up into
+// several small functions. You should've designed many of
+// these functions as a part of your assignment 3 design
+// submission. Define your functions here.
+
+int main() {
+
+}
